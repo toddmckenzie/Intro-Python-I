1ff0240ef8675c4284f086d842ea2c1ebc26dd0a
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-09-03 18:06:05 -0500

    part of dictionaries done/ comprehension

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -7,26 +7,26 @@ the list should be populated.
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
 """
-
+import math
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [i for i in range(1,6)]
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [math.pow(i, 3) for i in range(10)]
 
-print(y)
+print(y) 
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [(i[0:1].upper() + i[1:]) for i in a]
 
 print(y)
 
@@ -36,6 +36,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [i % 2 == 0 for i in x]
 
 print(y)
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -36,9 +36,13 @@ waypoints = [
 # Add a new waypoint to the list
 # YOUR CODE HERE
 
+waypoints.append({"lat": 1000, "lon": -100, "name": "a new place"})
+
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
+
+
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
